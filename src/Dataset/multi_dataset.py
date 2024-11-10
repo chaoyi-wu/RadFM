7fac8995612484ebf68e2707c85c099837bbe21b
chaoyi-wu <18706207256@163.com>
2024-11-10 14:56:38 +0800

    Update multi_dataset.py

--- src/Dataset/multi_dataset.py
+++ src/Dataset/multi_dataset.py
@@ -200,7 +200,7 @@ class multi_dataset(Dataset):
         print('spinexr_dataset loaded')
         
         ### VQA ###
-        pmcvqa_dataset = PMCVQA_Dataset(csv_path = '/gpfs/home/cs/leijiayu/wuchaoyi/wangyingjie/src/New_Dataset/data_csv/pmcvqa_train.csv')
+        pmcvqa_dataset = VQA_Dataset(csv_path = '/gpfs/home/cs/leijiayu/wuchaoyi/wangyingjie/src/New_Dataset/data_csv/pmcvqa_train.csv')
         self.dataset_reflect['pmcvqa_dataset'] = pmcvqa_dataset
         self.data_whole_2D = self.data_whole_2D +  [{'pmcvqa_dataset':i} for i in range(len(pmcvqa_dataset))]
         print('pmcvqa_dataset loaded')
@@ -211,12 +211,12 @@ class multi_dataset(Dataset):
         self.data_whole_2D = self.data_whole_2D +  [{'casereport_dataset':i} for i in range(len(casereport_dataset))]
         print('casereport_dataset loaded')
         
-        vqarad_dataset = PMCVQA_Dataset(csv_path = '/gpfs/home/cs/leijiayu/wuchaoyi/wangyingjie/src/New_Dataset/data_csv/vqarad_train.csv')
+        vqarad_dataset = VQA_Dataset(csv_path = '/gpfs/home/cs/leijiayu/wuchaoyi/wangyingjie/src/New_Dataset/data_csv/vqarad_train.csv')
         self.dataset_reflect['vqarad_dataset'] = vqarad_dataset
         self.data_whole_2D = self.data_whole_2D +  [{'vqarad_dataset':i} for i in range(len(vqarad_dataset))]
         print('vqarad_dataset loaded')
         
-        slake_dataset = PMCVQA_Dataset(csv_path = '/gpfs/home/cs/leijiayu/wuchaoyi/wangyingjie/src/New_Dataset/data_csv/slakevqa_train.csv')
+        slake_dataset = VQA_Dataset(csv_path = '/gpfs/home/cs/leijiayu/wuchaoyi/wangyingjie/src/New_Dataset/data_csv/slakevqa_train.csv')
         self.dataset_reflect['slake_dataset'] = slake_dataset
         self.data_whole_2D = self.data_whole_2D +  [{'slake_dataset':i} for i in range(len(slake_dataset))]
         print('slake_dataset loaded')
